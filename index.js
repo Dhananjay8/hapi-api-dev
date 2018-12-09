2081615f4b79d48ba05d21c737211e25589cc416
Dhananjay8 <dhananjay@ost.com>
2018-12-09 10:08:48 +0530

    added index and .gitignore files

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 'use strict';
 
 const Hapi = require('hapi');
+const mongoose = require('mongoose');
+
+// ES6 Promises
+mongoose.Promise = global.Promise;
+
+const dbuser = 'dhananjay97',
+  dbpassword = '#Dhananjay97';
+
+
+// Connect to mongodb
+mongoose.connect('mongodb://'+dbuser+':'+dbpassword+'@ds151382.mlab.com:51382/hapi_dev');
+mongoose.connection.once('open', function(){
+  console.log('Connection has been made, now make fireworks...');
+  done();
+}).on('error', function(error){
+  console.log('Connection error:', error);
+});
 
 const server = Hapi.server({
   port: 3000,
